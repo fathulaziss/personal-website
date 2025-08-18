ea74ee621f160a2e70e156667047b13f3b931dd5
Muhamad Fathul Azis <azisfathl@gmail.com>
2025-08-18 13:23:05 +0700

    update flutter version 3.35.1

--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -1,12 +1,11 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
-import 'dart:html' as html;
-
 import 'package:url_launcher/url_launcher.dart';
+import 'package:web/web.dart' as web;
 
 class AppUtils {
   static Future<void> openLink(String url) async {
-    html.window.open(url, '_blank');
+    web.window.open(url, '_blank');
   }
 
   static Future<void> launcher(Uri scheme) async {
